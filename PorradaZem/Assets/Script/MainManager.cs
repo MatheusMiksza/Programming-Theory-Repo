c91306443f5ed62c364b96ee77e7d29390d8e44d
Matheus Miksza <mathmiksza@gmail.com>
2024-04-19 17:57:11 -0300

    Criando menu principal

--- PorradaZem/Assets/Script/MainManager.cs
+++ PorradaZem/Assets/Script/MainManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance { get; private set; }
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
     }
 
     // Update is called once per frame
---
+++ PorradaZem/Assets/Script/Menu/MenuUiHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUiHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Button btn_play;
+
+    [SerializeField]
+    private Sprite[]btn_playSprit ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onClickBtnPlay()
+    {
+        
+    }
+}
